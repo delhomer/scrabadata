41e5e7e0333a9f739737120ac14c0dad80d00c5f
Raphaël Delhome <raphael.delhome@oslandia.com>
2019-08-19 13:55:40 +0200

    download: add geocoding features to retrieve club addresses

--- scrabadata/download.py
+++ scrabadata/download.py
@@ -8,14 +8,21 @@ https://www.ffsc.fr/classements.exporter.php?class=nat&date=YYYY-MM-DD
 
 https://www.ffsc.fr/clubs.php
 https://www.ffsc.fr/clubs.php?comite=<ID>
+
+- French areas:
+
+https://www.data.gouv.fr/fr/datasets/r/eb36371a-761d-44a8-93ec-3d728bec17ce
+https://www.data.gouv.fr/fr/datasets/r/36390bfd-437b-4abd-9713-d33a1673a700
 """
 
 from datetime import date
 from lxml import html
 from pathlib import Path
 
+import geocoder
 import pandas as pd
 import requests
+from shapely.geometry import Point
 
 
 RANKING_URL = "https://www.ffsc.fr/classements.exporter.php?class=nat&date={date}"
@@ -35,8 +42,8 @@ def get_ranking(ranking_date):
     ranking_date_str = ranking_date.strftime("%Y-%m-%d")
     url = RANKING_URL.format(date=ranking_date_str)
     output_rank_filepath = DATADIR / f"classement_{ranking_date_str}.csv"
+    resp = requests.get(url)
     with open(output_rank_filepath, "wb") as fobj:
-        resp = requests.get(url)
         fobj.write(resp.content)
 
 
@@ -68,3 +75,34 @@ def get_clubs(area_id):
     df_clubs = pd.DataFrame(club_list, columns=["id", "area"])
     output_club_filepath = DATADIR / f"clubs_comite_{area_id}.csv"
     df_clubs.to_csv(output_club_filepath, index=False)
+
+
+def add_address(area):
+    """Add an address column to a club dataframe identified by the area ID
+
+    Parameters
+    ----------
+    area : str
+        Area ID, as a single letter
+    """
+    df_filepath = DATADIR / f"clubs_comite_{area}.csv"
+    df_clubs = pd.read_csv(df_filepath)
+    df_clubs["address"] = ""
+    df_clubs.to_csv(df_filepath, index=False)
+
+
+def geocode(address):
+    """Geocode a club place identified by the main available address on the
+    FFSC website
+
+    Parameters
+    ----------
+    address : str
+        Main address of the club
+    """
+    if address == "":
+        return None
+    geocode = geocoder.osm(address)
+    if geocode.status == "ERROR - No results found":
+        return None
+    return Point(geocode.latlng)
